c8b2ccfc641a074dbba237feb177d77976c51956
himanshideveloper <156608936+himanshideveloper@users.noreply.github.com>
2024-02-07 19:24:42 +0530

    Add files via upload

---
+++ jsassignment4.js
@@ -0,0 +1,14 @@
+// Write a program to check whether a number is negative positive or zero. 
+
+let number = 32;
+if (number % 2 == 0) {
+    console.log("The number is positive");
+  }
+  
+  else if (number % 2 == 0) {
+    console.log("The number is negative");
+  }
+  
+  else {
+    console.log("The number is zero");
+  }
