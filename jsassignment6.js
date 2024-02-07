dc88e9f11ca29bc79312e177957ee4d5102c8045
himanshideveloper <156608936+himanshideveloper@users.noreply.github.com>
2024-02-07 20:01:56 +0530

    Add files via upload

---
+++ jsassignment6.js
@@ -0,0 +1,14 @@
+// Write a program to find maximum between two numbers using switch case. 
+
+let d = 20;
+let c = 19.5;
+
+switch(d > c){
+    case d: 
+    console.log('d is a maximum number');
+    break;
+    
+    case c :
+        console.log('c is a maximum number');
+        break;
+}
